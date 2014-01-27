7bdbe37af9c46cb6c5c53deae39728ce1961879a
Richard Jones <joneseh25@gmail.com>
2014-01-27 20:14:46 +0000

    Added a load_kernel function outline.

--- bootloader/stage2/src/stage2.c
+++ bootloader/stage2/src/stage2.c
@@ -1,5 +1,12 @@
 #include "elf.h"
 
+void load_kernel(void)
+{
+	/*	Read file from FAT into memory. */
+	/*	Find entry point. */
+	/*	Jump to entry point (passing in required info). */
+}
+
 void load_elf(void)
 {
 	
