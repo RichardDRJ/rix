0915c42f6d4e6340abbe5cb5189b0b31a5c770cf
Richard Jones <joneseh25@gmail.com>
2014-01-28 17:08:45 +0000

    Minor changes.

--- bootloader/stage2/src/stage2.c
+++ bootloader/stage2/src/stage2.c
@@ -1,13 +1,14 @@
 #include "elf.h"
+#include "readfat.h"
+
+#define KERNEL_OFFSET (void*)0x100000
+#define KERNEL_NAME "kernel.bin"
 
 void load_kernel(void)
 {
 	/*	Read file from FAT into memory. */
+	read_file_to(KERNEL_NAME, KERNEL_OFFSET);
+	
 	/*	Find entry point. */
 	/*	Jump to entry point (passing in required info). */
-}
-
-void load_elf(void)
-{
-	
 }
